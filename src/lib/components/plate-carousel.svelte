ddf0f857dc3621d7b968561b72033e9b3d68e50a
Martín Amarillo <46434353+MartinAmarillo2505@users.noreply.github.com>
2026-01-25 01:28:13 -0300

    feat: add new Svelte component for displaying a carousel of plates with their details.

--- src/lib/components/plate-carousel.svelte
+++ src/lib/components/plate-carousel.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import * as Carousel from '$lib/components/ui/carousel';
 	import { formatTime } from '$lib/utils';
 	import { Box, Clock, Weight } from 'lucide-svelte';
 
@@ -19,32 +18,30 @@
 
 <section class="mb-2 rounded bg-secondary p-2">
 	<h1 class="text-xl font-bold">Placas</h1>
-	<Carousel.Root class="rounded bg-primary-foreground p-2">
-		<Carousel.Content>
-			{#each plates as plate}
-				<Carousel.Item class="basis-auto">
-					<img
-						class="aspect-square w-32 rounded object-cover"
+	<section class="flex gap-4 overflow-x-auto rounded bg-primary-foreground p-2">
+		{#each plates as plate, index}
+			<article class="basis-auto">
+				<img
+					class="aspect-square w-32 rounded object-cover"
 					src={`/uploads/${plate.thumbnailSha1}`}
 					loading="lazy"
-						alt={`Imagen de la placa ${plate.name}`} />
-					<p class="font-bold">{plate.name}</p>
-					<p class="flex gap-2 text-xs text-nowrap">
-						<span class="flex items-center gap-0.5">
-							<Box size="1em" />
-							{plate.objects}
-						</span>
-						<span class="flex items-center gap-0.5">
-							<Clock size="1em" />
-							{formatTime(plate.timeSeconds)}
-						</span>
-						<span class="flex items-center gap-0.5">
-							<Weight size="1em" />
-							{plate.weightGrams}g
-						</span>
-					</p>
-				</Carousel.Item>
-			{/each}
-		</Carousel.Content>
-	</Carousel.Root>
+					alt={`Imagen de la placa ${plate.name}`} />
+				<p class="font-bold">{plate.name || `Placa ${index + 1}`}</p>
+				<p class="flex gap-2 text-xs text-nowrap">
+					<span class="flex items-center gap-0.5">
+						<Box size="1em" />
+						{plate.objects}
+					</span>
+					<span class="flex items-center gap-0.5">
+						<Clock size="1em" />
+						{formatTime(plate.timeSeconds)}
+					</span>
+					<span class="flex items-center gap-0.5">
+						<Weight size="1em" />
+						{plate.weightGrams}g
+					</span>
+				</p>
+			</article>
+		{/each}
+	</section>
 </section>
