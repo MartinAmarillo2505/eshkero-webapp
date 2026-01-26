e2f9a6bd3cf850adb2923675975e9c8c2173054a
Martín Amarillo <46434353+MartinAmarillo2505@users.noreply.github.com>
2026-01-25 22:13:21 -0300

    fix(build): only connect to database when not in building process

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,9 +2,15 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { building } from '$app/environment';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+type DB = ReturnType<typeof drizzle<typeof schema>>;
 
-const client = postgres(env.DATABASE_URL);
+function connectDB() {
+  if (building) return {} as DB;
 
-export const db = drizzle(client, { schema });
+  if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+  return drizzle(postgres(env.DATABASE_URL), { schema });
+}
+
+export const db = connectDB();
