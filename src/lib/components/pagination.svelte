54995f67b7890dbc7acc08a49a52c220fe9f94ff
Martín Amarillo <46434353+MartinAmarillo2505@users.noreply.github.com>
2026-01-28 13:23:37 -0300

    feat: add searchbar and product pagination, along with limited queries

---
+++ src/lib/components/pagination.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import * as Pagination from '$lib/components/ui/pagination';
+	import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-svelte';
+	import { Pagination as PaginationPrimitive } from 'bits-ui';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+
+	let props: Omit<PaginationPrimitive.RootProps, 'page'> = $props();
+
+	const getPage = () => Number(page.url.searchParams.get('page')) || 1;
+
+	const setPage = (pageNum: number) => {
+		const url = new URL(page.url);
+		url.searchParams.set('page', pageNum.toString());
+		goto(url, { replaceState: true, keepFocus: true, noScroll: true });
+	};
+</script>
+
+<Pagination.Root bind:page={getPage, setPage} {...props}>
+	{#snippet children({ pages, currentPage })}
+		<Pagination.Content>
+			<Pagination.PrevButton>
+				<ChevronLeftIcon />
+			</Pagination.PrevButton>
+			{#each pages as page (page.key)}
+				<Pagination.Item>
+					{#if page.type === 'ellipsis'}
+						<Pagination.Ellipsis />
+					{:else}
+						<Pagination.Link {page} isActive={currentPage === page.value}>
+							{page.value}
+						</Pagination.Link>
+					{/if}
+				</Pagination.Item>
+			{/each}
+			<Pagination.NextButton>
+				<ChevronRightIcon />
+			</Pagination.NextButton>
+		</Pagination.Content>
+	{/snippet}
+</Pagination.Root>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -2,16 +2,21 @@
 	import CreateProduct from '$lib/components/create-product.svelte';
 	import ProductCard from '$lib/components/product-card.svelte';
 	import SearchBar from '$lib/components/search-bar.svelte';
+	import Pagination from '$lib/components/pagination.svelte';
 
 	const { data } = $props();
 </script>
 
 <CreateProduct />
 
-<SearchBar placeholder="Buscar productos..." results={data.products.length} />
+<SearchBar placeholder="Buscar productos..." count={data.count} perPage={data.perPage} />
 <section
-	class="mx-auto grid w-full max-w-300 grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-x-2 gap-y-4">
+	class="mx-auto mb-4 grid w-full max-w-300 grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-x-2 gap-y-4">
 	{#each data.products as product}
 		<ProductCard {...product} />
+	{:else}
+		<p class="col-span-full text-center p-4">No se encontraron resultados</p>
 	{/each}
 </section>
+
+<Pagination count={data.count} perPage={data.perPage} />
